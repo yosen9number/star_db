edafd702f9245fbd20b2ca5d92d7c1d4a432af14
Naira Lalayan <nlalayan@ivi.ru>
2020-03-10 19:32:06 +0300

    section-7 обновлен компонент деталей о персонаже, с данными, приходящимии из api

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,29 +1,91 @@
 import React, { Component } from 'react';
 
 import './person-details.css';
+import SwapiService from "../../services/swapi-service";
 
 export default class PersonDetails extends Component {
 
+  swapiService = new SwapiService();
+
+  state = {
+    person: null
+  };
+
+  componentDidMount () {
+    this.updatePerson();
+  }
+
+  updatePerson() {
+    const {personId} = this.props;
+    if (!personId) {
+      return;
+    }
+
+    this.swapiService
+        .getPerson(personId)
+        .then((person) => {
+          this.setState({person});
+        })
+  }
+
   render() {
+    if (!this.state.person) {
+      return <span>Select a person</span>
+    }
+
+    const { id, name, birthYear, gender, homeworld, skinColor,
+      height, mass, hairColor, eyeColor, species, starships
+    } = this.state.person;
+
     return (
       <div className="person-details card">
         <img className="person-image"
-          src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+          alt={`Character: ${name}`}
+        />
 
         <div className="card-body">
-          <h4>R2-D2</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Gender</span>
-              <span>male</span>
+              <span>{gender}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Birth Year</span>
-              <span>43</span>
+              <span>{birthYear}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Height</span>
+              <span>{height}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Mass</span>
+              <span>{mass}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Hair Color</span>
+              <span>{hairColor}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Eye Color</span>
-              <span>red</span>
+              <span>{eyeColor}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Skin Color</span>
+              <span>{skinColor}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Home</span>
+              <span>{homeworld}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Species</span>
+              <span>{species}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Starships</span>
+              <span>{starships}</span>
             </li>
           </ul>
         </div>
