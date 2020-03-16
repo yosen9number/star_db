2e185e9447513147b2a30d908872cb6fda2735fb
Naira Lalayan <nlalayan@ivi.ru>
2020-03-16 23:01:39 +0300

    section-8 добавлены propsTypes и обязательные свойства

--- src/components/row/row.js
+++ src/components/row/row.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import './row.css';
 
@@ -14,6 +15,11 @@ const Row = ({left, right}) => {
             </div>
         </div>
     )
+};
+
+Row.propTypes = {
+    left: PropTypes.node,
+    right: PropTypes.node,
 };
 
 export default Row;
