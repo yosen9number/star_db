f79c3944c30e67a685673fe1568f0445b13715af
Naira Lalayan <nlalayan@ivi.ru>
2020-03-16 22:52:33 +0300

    section-8 propTypes

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -8,6 +8,22 @@ import ErrorIndicator from "../error-indicator";
 
 export default class RandomPlanet extends Component {
 
+    static defaultProps = {
+        updateInterval: 10000
+    };
+
+    static propTypes = {
+        updateInterval: (props, propName, componentName) => {
+            const value = props[propName];
+
+            if (typeof value === 'number' && !isNaN(value)) {
+                return null;
+            }
+
+            return new TypeError(`${componentName}: ${propName} must be number`);
+        }
+    };
+
     swapiService = new SwapiService();
 
     state = {
@@ -17,8 +33,9 @@ export default class RandomPlanet extends Component {
     };
 
     componentDidMount() {
+        const { updateInterval } = this.props;
         this.updatePlanet();
-        this.interval = setInterval(this.updatePlanet, 10000);
+        this.interval = setInterval(this.updatePlanet, updateInterval);
     }
 
     componentWillUnmount() {
